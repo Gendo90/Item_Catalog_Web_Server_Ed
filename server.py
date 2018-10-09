ec6240c60c66170b7ac6d10f1ed199be2792d54a
Patrick Gendotti <patrick.gendotti@gmail.com>
2018-10-09 12:31:03 -0700

    Add basic server.py script to run the website using Flask and SQLAlchemy

---
+++ server.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Genre, BookItem
+
+engine = create_engine('sqlite:///booklist.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
+
+# Main page for the website
+@app.route('/')
+def mainPage():
+    return render_template('index.html')
+
+
+# code from project.py listing books by genre
+@app.route("/genres/<int:genre_id>/")
+def listGenre(genre_id):
+    genre = session.query(Genre).filter_by(id = genre_id).one()
+    genreBooks = session.query(BookItem).filter_by(genre_id = genre.id).all()
+
+    return render_template('genre_list.html', genre=genre, bookList=genreBooks)
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=5500)
