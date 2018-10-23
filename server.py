b43e99b5eea8e3293447b952b5c51e2d92ab6069
Patrick Gendotti <patrick.gendotti@gmail.com>
2018-10-23 16:15:16 -0700

    feat: Enable login using Google account

--- server.py
+++ server.py
@@ -9,6 +9,22 @@ from database_setup import Base, SuperCategory, Genre, BookItem
 from flask import session as login_session
 import random, string
 
+# import packages for google oauth login
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+import json
+from flask import make_response
+import requests
+
+# Some global variables that are used in the GConnect function
+# NOTE: need to download the 'client_secrets.json' from the google account AFTER
+# updating the certification on the google account to allow redirection to BOTH
+# http://localhost:5000/login and http://localhost:5000/gconnect or it will not work
+CLIENT_ID = json.loads(
+    open('client_secrets.json', 'r').read())['web']['client_id']
+APPLICATION_NAME = "Book Collector App"
+
 engine = create_engine('sqlite:///booklist.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind = engine)
@@ -24,19 +40,110 @@ def mainPage():
     return render_template('index-logged-in.html')
 
 # login page for the website
-@app.route('/login/')
-def loginPage():
-    return render_template('login.html')
-
 # Create a state token to prevent request forgery.
 # Store it in the session for later validation
 # Show login verification page
-@app.route('/login/verify')
-def showLogin():
+@app.route('/login/')
+def loginPage():
     state = ''.join(random.choice(string.ascii_uppercase +
     string.digits) for x in range(32))
     login_session['state'] = state
-    return ("The current session state is {}").format(login_session['state'])
+    return render_template('login.html', STATE=state)
+
+# GConnect code to authenticate user using the google API
+# With the "client secret" and client # ID
+# ALso verifies that the request comes from the browser with the same anti-forgery state
+# token as the one provided by the browser.
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    # Validate state token
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    # Obtain authorization code
+    code = request.data
+
+    try:
+        # Upgrade the authorization code into a credentials object
+        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(
+            json.dumps('Failed to upgrade the authorization code.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Check that the access token is valid.
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+    # If there was an error in the access token info, abort.
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is used for the intended user.
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(
+            json.dumps("Token's user ID doesn't match given user ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is valid for this app.
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        print("Token's client ID does not match app's.")
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    stored_access_token = login_session.get('access_token')
+    #stored_gplus_id = login_session.get('gplus_id')
+    #if stored_access_token is not None and gplus_id == stored_gplus_id:
+    #    response = make_response(json.dumps('Current user is already connected.'),
+    #                             200)
+    #    response.headers['Content-Type'] = 'application/json'
+    #    return response
+
+    # Store the access token in the session for later use.
+    login_session['access_token'] = credentials.access_token
+    login_session['gplus_id'] = gplus_id
+
+    # Get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+
+    data = answer.json()
+    login_session['provider'] = 'google'
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    flash("you are now logged in as %s" % login_session['username'])
+    print("done!")
+
+    #checks if this User already exists in the database - if not, adds them!
+    #if(getUserID(login_session['email']) == None):
+    #    createUser(login_session)
+
+    #login_session['user_id'] = getUserID(login_session['email'])
+
+
+    return output
 
 # First of three super-categories that link to different pages - refactor
 # later to make the three super-categories part of a table in the database
